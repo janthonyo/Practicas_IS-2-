fbbca6c12d9b3ccb1202255f9fa6fef68f45908d
Manuel Mariscal <i72marom@uco.es>
2018-11-30 21:24:48 +0100

    Añadida la clase BD

---
+++ Practica4/Codigo/bd.h
@@ -0,0 +1,23 @@
+// bd.h
+// define las funciones de la clase y contiene los comentarios sobre su funcionamiento
+
+#ifndef BD_H
+#define BD_H
+
+	#include "profesor.h"
+	#include "alumno.h"
+	#include <list>
+	#include <string>
+
+	class bd {
+		private:
+			list <Profesor> profesores_;
+			list <Alumno> alumnos_;
+			string nombre_fichero_;
+		
+		public:
+			// set para la parte privada
+			void setProfesor	
+	};
+
+#endif
