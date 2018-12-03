e2b4553ef71d7d9cf6760e2f087c71e2d97b6bc2
Manuel Mariscal <i72marom@uco.es>
2018-12-03 10:12:46 +0100

    bd.h version: 1

--- Practica4/Codigo/bd.h
+++ Practica4/Codigo/bd.h
@@ -8,19 +8,19 @@
 	#include <list>
 	#include <string>
 
-	class bd {
+	class BD {
 		private:
 			list <Alumno> alumnos_; // lista que contiene los alumnos de la clase de is
 			string nombre_fichero_; // variable que guarda el nombre de la base de datos que se quiere cargar
 		
 		public:
 			// observadores y modificadores para la lista de alumnos
-			void setAlumnos(Alumno alumnos);
-			Alumno getAlumnos();
+			void setAlumnos();
+			inline Alumno getAlumnos() { return alumnos_; }
 
 			// observadores y modificadores para el nombre del fichero
-			void setNombreFichero(string nombre);
-			string getNombreFichero();
+			inline void setNombreFichero(string nombre) { nombre_fichero_ = nombre; }
+			inline string getNombreFichero() { return nombre_fichero_; }
 
 			// bool introducirAlumno(Alumno newAlumno)
 			// recibe como parametro un objeto alumno y lo introduce en la base de datos
