c04963c8e8a7a4dc9616dca208ea92724a28fd17
Jose Antonio Martinez Molina <i72marmj@uco.es>
2018-12-08 02:19:42 +0100

    Añadidas funcionalidades del profesor

--- Practica4/Codigo/Profesor.cc
+++ Practica4/Codigo/Profesor.cc
@@ -1,21 +1,48 @@
-#include <string.h>
+#include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include <fstream>
 #include "Profesor.h"
 
-using namespace std;
-
 int Profesor::login(std::string usuario, std::string contrasena)
 {
+	char aux[50];
+	std::string user_file;
+	std::string key_file;
+	int rol_file;
+	int encontrado=0;
 
-	if((usuario=="usuario")&&(contrasena=="12345")) return 1;	
+	//Estructura del fichero: usuario, contraseña, rol.
 
-	else if((usuario=="usuario")&&(contrasena=="54321")) return 2;
+	std::ifstream fichero("profesores.txt");
 
-	system("clear");
+	while((fichero.getline(aux, 50, ','))&&(encontrado==0))
+	{
+		user_file=aux;
+		fichero.getline(aux, 50, ',');
+		key_file=aux;
+		fichero.getline(aux, 50);
+		rol_file=atoi(aux);
 
-	cout<<"Nombre de usuario o contraseña incorrectos."<<endl<<endl;
+		if((user_file==usuario)&&(key_file==contrasena))
+		{
+			usuario_=user_file;
+			rol_=rol_file;
+			encontrado=1;
+		}
+	}
 
-	return 0;
+	fichero.close();
 
-};
+	if(encontrado==0)
+	{
+		std::cout<<"Nombre de usuario o contraseña incorrectos."<<std::endl<<std::endl;
+		return 0;
+	}
+
+	if(encontrado==1) return rol_;
+
+
+}
