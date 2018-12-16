cc32ab04bd6bc4ae22ce12bb15362852d6488d1c
Manuel Mariscal <i72marom@uco.es>
2018-12-16 18:36:54 +0100

    copiaExterna.h

--- Practica4/Codigo/copiaExterna.h
+++ Practica4/Codigo/copiaExterna.h
@@ -11,6 +11,12 @@
 	// carga la copia de seguridad en git
 	void crearCopiaExterna(string nombre);
 
+	// bool cargarCopiaExterna(string nombre);
+	// recibe como parametro el nombre de la base de datos sin la extension .bin
+	// crea una copia de la base de datos externa en el directorio /tmp/copia_externa
+	// si no existe el directorio lo crea
+	// si no esta inicializado en git, lo inicializa
+	// carga la copia de seguridad en el sistema
 	bool cargarCopiaExterna(string nombre);
 
 #endif
